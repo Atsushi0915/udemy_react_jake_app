5bb9c619a19669d323916bee35a94a6b87bcdde3
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-07-12 06:58:54 +0900

    ルーティング 作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Button, ChakraProvider} from '@chakra-ui/react';
+import { ChakraProvider} from '@chakra-ui/react';
+import { Router } from './router/Router';
+
 import { theme } from './theme/theme';
 
 function App() {
   return (
     <ChakraProvider theme={theme} >
-      <Button colorScheme="teal">ボタン</Button>
-      <h1>aaaas</h1>
+      <Router />
     </ChakraProvider>
   );
 }
