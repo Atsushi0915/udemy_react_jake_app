8c5a2e84454a119a98887a46cdd57f7af7348e0a
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-07-22 06:57:52 +0900

    UserCard.tsx 作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,9 +1,17 @@
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { FC, memo } from "react";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: FC = memo(() => {
   return (
-    <>
-      <p>ユーザー管理ページです。</p>
-    </>
+    <Wrap p={{ base: 4, md: 10}}>
+      <WrapItem>
+        <UserCard 
+          imageUrl={"https://source.unsplash.com/random"}
+          userName={"あつし"}
+          fullName={"Wada Atushi"}  
+        />
+      </WrapItem>
+    </Wrap>
   )
 })
